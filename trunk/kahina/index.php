dc2d2d74121d539cf05cae5445cd9e46b96eed4a
azaounkahina@gmail.com <azaounkahina@gmail.com@2ba35f43-1ee0-6f9c-fa09-96ac1c26f180>
2011-11-01 22:58:48 +0000

    mail to all

--- trunk/kahina/index.php
+++ trunk/kahina/index.php
@@ -10,8 +10,10 @@
  <?php
  //récuperation du flux rss
     $simple_xml = simplexml_load_file ( "http://picasaweb.google.com/data/feed/base/user/107353736179759429408/albumid/5659262403796066625?alt=rss&kind=photo&hl=en_US" );
+	$mails[]='';
 	foreach ( $simple_xml->channel->item as $person ) {
 		$tableau=split(", ",$person->title);
+		array_push($mails,$tableau[2]);
 		$mail=$tableau[2];?>
 			<div class="imge">
 				<a href='mailto:<?php echo $mail;?>'><img src="<?php echo $person->enclosure['url'];?>" width="150" height="150" /></a>
@@ -20,5 +22,6 @@
 				</div>
 			</div> 
 	<?php } ?>
+	<p class="mails"><a href='mailto:<?php echo implode(",",$mails);?>'>Envoyer un mail &agrave; tous</a></p>
 </body>
 </html>
